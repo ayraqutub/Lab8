6cff394a733d3e87572700f7a77dbe8cb3890c54
Ayra Qutub <ayra4027@gmail.com>
2024-10-25 13:33:40 -0600

    Has city

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,6 +1,8 @@
 package com.example.lab8;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -31,4 +33,13 @@ public class CustomListTest {
         assertEquals(list.getCount(),listSize + 1);
     }
 
+    @Test
+    public void testHasCity(){
+        CustomList cityList = MockCityList();
+        City city = new City("Regina", "Saskatchewan");
+        cityList.add(city);
+        assertTrue(cityList.hasCity(city));
+        City city2 = new City("Calgary", "Alberta");
+        assertFalse(cityList.hasCity(city2));
+    }
 }
