8a49221c0b6c8fc7fae1141b3965761ab525d01d
Ayra Qutub <ayra4027@gmail.com>
2024-10-25 13:37:10 -0600

    Has city

--- app/src/main/java/com/example/lab8/City.java
+++ app/src/main/java/com/example/lab8/City.java
@@ -1,6 +1,8 @@
 package com.example.lab8;
 
-public class City {
+import java.util.Objects;
+
+public class City implements Comparable{
 
     private String city;
     private String province;
@@ -16,5 +18,24 @@ public class City {
 
     String getProvinceName(){
         return this.province;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        City city = (City) o;
+        return this.city.compareTo(city.getCityName()); // this.city refers to the city name
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city1 = (City) o;
+        return Objects.equals(city, city1.city) && Objects.equals(province, city1.province);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, province);
     }
 }
